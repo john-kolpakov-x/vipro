81187036907caa0183c4e9e4cd53ccbeb414aaa5
pompei <john.kolpakov.x@gmail.com>
2016-07-09 21:48:59 +0600

    Added ComplexTest

---
+++ vipro/test_src/kz/pompei/vipro/display/impl/ComplexTest.java
@@ -0,0 +1,45 @@
+package kz.pompei.vipro.display.impl;
+
+import kz.pompei.vipro.display.BracketsType;
+import kz.pompei.vipro.display.DisplayExpr;
+import kz.pompei.vipro.display.DisplayPortImpl;
+import org.testng.annotations.Test;
+
+import java.awt.Color;
+
+import static kz.pompei.vipro.display.impl.TestDisplayUtil.displayToFile;
+
+public class ComplexTest {
+  @Test
+  public void name() throws Exception {
+    DisplayExpr expr = getDisplayExpr();
+
+    System.out.println(expr);
+
+    DisplayPortImpl port = new DisplayPortImpl();
+    expr.setPort(port);
+
+    displayToFile(expr, port, getClass().getSimpleName());
+
+  }
+
+  private DisplayExpr getDisplayExpr() {
+    Color color = new Color(0, 0, 0);
+
+    DisplayText a = new DisplayText(0, "α", color, false, false);
+    DisplayText eq = new DisplayText(0, "=", color, false, false);
+
+    DisplayText sin = new DisplayText(0, "sin", color, false, false);
+    DisplayText cos = new DisplayText(0, "cos", color, false, false);
+    DisplayText tg = new DisplayText(0, "tg", color, false, false);
+
+    DisplayBrackets b_a = new DisplayBrackets(a, BracketsType.ROUND, color);
+
+    DisplayExpr top = DisplayLeaning.displayOrder(sin, b_a);
+    DisplayExpr bot = DisplayLeaning.displayOrder(cos, b_a);
+
+    DisplayDiv div = new DisplayDiv(0, top, bot, 0.04f, color);
+
+    return DisplayLeaning.displayOrder(tg, b_a, eq, div);
+  }
+}
--- vipro/test_src/kz/pompei/vipro/display/impl/TestDisplayUtil.java
+++ vipro/test_src/kz/pompei/vipro/display/impl/TestDisplayUtil.java
@@ -5,6 +5,8 @@ import kz.pompei.vipro.display.DisplayPortImpl;
 import kz.pompei.vipro.display.Size;
 
 import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -23,7 +25,10 @@ public class TestDisplayUtil {
       Size size = displayExpr.size();
       BufferedImage image = new BufferedImage(size.width + 20, size.height() + 20, BufferedImage.TYPE_INT_ARGB);
 
-      port.setGraphics(image.createGraphics());
+      Graphics2D g = image.createGraphics();
+      g.setColor(Color.white);
+      g.fillRect(0, 0, image.getWidth(), image.getHeight());
+      port.setGraphics(g);
 
       displayExpr.displayTo(10, 10 + size.top);
 
