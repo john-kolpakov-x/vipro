71799205df239ca178bcc73cdfe408884ab01e9e
pompei <john.kolpakov.x@gmail.com>
2016-08-21 11:15:07 +0600

    Fix some tmp modifications

--- vipro/test_src/kz/pompei/vipro/tmp/AbstractClassA.java
+++ vipro/test_src/kz/pompei/vipro/tmp/AbstractClassA.java
@@ -4,6 +4,13 @@ public abstract class AbstractClassA {
 
   public abstract int abstractMethod(int x, int y);
 
+  /**
+   * Это супер описание этого метода
+   *
+   * @param X это переменная икс
+   * @param Y это аргумент игрек
+   * @return чё-то надо вернуть
+   */
   public String callAbstract(String X, String Y) {
     return "" + abstractMethod(Integer.parseInt(X), Integer.parseInt(Y));
   }
