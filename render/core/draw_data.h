b6ba9d0ec35d5c2ed996845684e29501ba1b6e9d
pompei <john.kolpakov.x@gmail.com>
2017-12-16 18:05:58 +0600

    Переведено два измерения в три

--- render/core/draw_data.h
+++ render/core/draw_data.h
@@ -7,7 +7,7 @@
 #include <array>
 
 struct Vertex {
-  glm::vec2 pos;
+  glm::vec3 pos;
   glm::vec3 color;
 
   static VkVertexInputBindingDescription getBindingDescription() {
@@ -23,7 +23,7 @@ struct Vertex {
 
     attributeDescriptions[0].binding = 0;
     attributeDescriptions[0].location = 0;
-    attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+    attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
     attributeDescriptions[0].offset = static_cast<uint32_t>(offsetof(Vertex, pos));
 
     attributeDescriptions[1].binding = 0;
